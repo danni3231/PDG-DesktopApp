0e5a1b867151884fa628a165dbc4885719ddd956
Daniel Rojas <53954296+danni3231@users.noreply.github.com>
2022-02-26 09:41:27 -0500

    Revert "added NavBar component"
    This reverts commit b49d0ccf0327861136489390bc01410b6c45ed1a.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Components/App/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
